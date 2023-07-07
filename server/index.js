e5fba53952c16a6852921c5aadb049803b3fa8ea
bxljoy <259500313@qq.com>
2023-07-08 00:06:50 +0700

    first commit my-social-media application

---
+++ server/index.js
@@ -0,0 +1,26 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import mongoose from 'mongoose';
+import postRoutes from './routes/posts.js';
+import 'dotenv/config';
+
+
+const app = express();
+app.use('/posts', postRoutes);
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+const url1 = "mongodb+srv://";
+const url2 = "@cluster0.djq235y.mongodb.net/";
+const url3 = "?retryWrites=true&w=majority";
+const userPassword = process.env.MONGODB_ATLAS_USER + ":" + process.env.MONGODB_ATLAS_PASSWORD;
+const mongodbName = process.env.MONGODB_NAME;
+// const url = url1 + userPassword + url2 + mongodbName + url3;
+const url = url1 + userPassword + url2 + url3;
+const PORT = process.env.PORT || 2000;
+mongoose.connect(url)
+    .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
+    .catch((error) => console.log(error.message));
