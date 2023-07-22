ee119ed5cc81b24aadd1209d4a5d6aa701b8749d
bxljoy <259500313@qq.com>
2023-07-22 14:23:29 +0700

    fetch all posts from mongodb and show posts on the page

--- client/src/components/Posts/Posts.js
+++ client/src/components/Posts/Posts.js
@@ -1,21 +1,29 @@
 import React from "react";
 import Post from "./Post/Post";
 import { useSelector } from "react-redux";
+import { Grid, CircularProgress } from "@material-ui/core";
 
 import postsStyles from './styles';
 
 const Posts = () => {
-    const posts = useSelector((state) => state.posts);
+    const posts = useSelector((state) => state.postsReducer);
     const classes = postsStyles();
 
     console.log(posts);
 
+
     return (
-        <>
-            <h1>POSTS</h1>
-            <Post />
-            <Post />
-        </>
+        !posts.length ? <CircularProgress /> : (
+            <Grid className={classes.container} container alignItems="stretch" spacing={3}>
+                {
+                    posts.map(post => (
+                        <Grid key={post._id} item xs={12} sm={6}>
+                            <Post post={post} />
+                        </Grid>
+                    ))
+                }
+            </Grid>
+        )
     );
 }
 
