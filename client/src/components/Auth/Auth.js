d72a0a67e3772d9113240d6462b5edf1110dc664
bxljoy <259500313@qq.com>
2023-07-29 14:49:24 +0700

    trying to add Google Sign In for React but Stucking

--- client/src/components/Auth/Auth.js
+++ client/src/components/Auth/Auth.js
@@ -1,12 +1,104 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { Avatar, Grid, Paper, Button, Typography, Container } from '@material-ui/core';
+import { GoogleLogin } from 'react-google-login';
+import LockOutLinedIcon from '@material-ui/icons/LockOutlined';
+import useStyles from './styles';
+import Input from './Input'; 
+import MyIcon from './MyIcon';
+
 
 const Auth = () => {
-    const state = null;
+    const classes = useStyles();
+    const [isSignup, setIsSignup] = useState(false);
+
+    const [showPassword, setShowPassword] = useState(false);
+
+    const handleShowPassword = () => setShowPassword((prevShowPassword) => !prevShowPassword);
+
+    const handleSubmit = () => {
+
+    };
+
+    const handleChange = () => {
+
+    };
+
+    const switchMode = () => {
+        setIsSignup((prevMode) => !prevMode);
+        setShowPassword(false);
+    };
+
+    const googleSuccess = async (res) => {
+        console.log(res);
+    };
+
+    const googleFailure = (error) => {
+        console.log(error); 
+        console.log('Google Sign In was unsuccessful. Try Again Later');
+    };
+
     return (
-        <div>
-            Auth
-        </div>
+        <Container component="main" maxWidth="xs">
+            <Paper className={classes.paper} elevation={3}>
+                <Avatar className={classes.avatar}>
+                    <LockOutLinedIcon />
+                </Avatar>
+                <Typography variant='h5'>{isSignup ? 'Sign Up' : 'Sign In'}</Typography>
+                <form className={classes.form} onSubmit={handleSubmit}>
+                    <Grid container spacing={2}>
+                        {
+                            isSignup && (
+                                <>
+                                    <Input name='firstName' label='First Name' handleChange={handleChange} autoFocus half />
+                                    <Input name='lastName' label='Last Name' handleChange={handleChange} half />
+                                </>
+                            )
+                        }
+                        <Input name='email' label='Email Address' handleChange={handleChange} type='email'  />
+                        <Input name='password' label='Password' handleChange={handleChange} type={showPassword ? 'text' : 'password'} handleShowPassword={handleShowPassword} />
+                        { isSignup && <Input name='confirmPassword' label='Repeat Password' handleChange={handleChange} type='password' /> }
+                    </Grid>
+                    <Button type='submit' fullWidth variant='contained' color='primary' className={classes.submit}>
+                        {isSignup ? 'Sign Up' : 'Sign In'}
+                    </Button>
+                    <GoogleLogin 
+                        clientId='123276089249-3m11ggv0autcvreun3pgqh395ajkrgfk.apps.googleusercontent.com'
+                        render= {(renderProps) => (
+                            <Button 
+                            className={classes.googleButton} 
+                            color='primary' 
+                            fullWidth 
+                            onClick={renderProps.onClick} 
+                            disabled={renderProps.disabled} 
+                            startIcon={<MyIcon />} 
+                            variant='contained'
+                            >
+                                Google Sign In
+                            </Button>
+                        )}
+                        onSuccess={googleSuccess}
+                        onFailure={googleFailure}
+                        cookiePolicy='single_host_origin'
+                    />
+                    <Grid container justifyContent='flex-end'>
+                        <Grid item>
+                            <Button onClick={switchMode}>
+                                { isSignup ? 'Already have an account? Sign In' : "Don't have an account? Sign Up" }
+                            </Button>
+                        </Grid>
+                    </Grid>
+                </form>
+            </Paper>
+        </Container>
     )
 }
 
 export default Auth;
+
+// {
+//     "error": "idpiframe_initialization_failed",
+//     "details": "You have created a new client application 
+//      that uses libraries for user authentication or authorization 
+//      that are deprecated. New clients must use the new libraries instead. 
+//      See the [Migration Guide](https://developers.google.com/identity/gsi/web/guides/gis-migration) for more information."
+// }
