01e008002fea66d753f2746eb9380e6bf94bfba2
bxljoy <259500313@qq.com>
2024-03-23 02:14:20 +0100

    Update index.js with proper formatting and OAuth client ID

--- client/src/index.js
+++ client/src/index.js
@@ -1,22 +1,22 @@
-import React from 'react';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware, compose } from 'redux';
-import thunk from 'redux-thunk';
-import { createRoot } from 'react-dom/client';
-import reducers from './reducers';
-import { GoogleOAuthProvider } from '@react-oauth/google';
+import React from "react";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware, compose } from "redux";
+import thunk from "redux-thunk";
+import { createRoot } from "react-dom/client";
+import reducers from "./reducers";
+import { GoogleOAuthProvider } from "@react-oauth/google";
 
-import App from './App';
-import './index.css';
+import App from "./App";
+import "./index.css";
 
-const store = createStore(reducers, compose(applyMiddleware(thunk)))
+const store = createStore(reducers, compose(applyMiddleware(thunk)));
 
-const container = document.getElementById('root');
+const container = document.getElementById("root");
 const root = createRoot(container);
 root.render(
-    <Provider store = { store }>
-        <GoogleOAuthProvider clientId='123276089249-3m11ggv0autcvreun3pgqh395ajkrgfk.apps.googleusercontent.com'>
-            <App />
-        </GoogleOAuthProvider>
-    </Provider>
+  <Provider store={store}>
+    <GoogleOAuthProvider clientId="394704547593-k87e89cfhr41qo672f5r7egqn2orpbjo.apps.googleusercontent.com">
+      <App />
+    </GoogleOAuthProvider>
+  </Provider>
 );
