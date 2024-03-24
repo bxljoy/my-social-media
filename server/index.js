11eba1e2251affc787177214b4d98863624a95b1
bxljoy <259500313@qq.com>
2024-03-24 23:54:39 +0100

    Refactor server/index.js file

--- server/index.js
+++ server/index.js
@@ -1,12 +1,11 @@
-import express from 'express';
-import bodyParser from 'body-parser';
-import cors from 'cors';
-import mongoose from 'mongoose';
-import postRoutes from './routes/posts.js';
-import googleAuthRoutes from './routes/googleAuth.js';
-import userRoutes from './routes/users.js';
-import 'dotenv/config';
-
+import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
+import mongoose from "mongoose";
+import postRoutes from "./routes/posts.js";
+import googleAuthRoutes from "./routes/googleAuth.js";
+import userRoutes from "./routes/users.js";
+import "dotenv/config";
 
 const app = express();
 
@@ -14,18 +13,21 @@ app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
-app.use('/posts', postRoutes);
-app.use('/auth', googleAuthRoutes);
-app.use('/user', userRoutes);
+app.use("/posts", postRoutes);
+app.use("/auth", googleAuthRoutes);
+app.use("/user", userRoutes);
 
-const url1 = "mongodb+srv://";
-const url2 = "@cluster0.djq235y.mongodb.net/";
-const url3 = "?retryWrites=true&w=majority";
-const userPassword = process.env.MONGODB_ATLAS_USER + ":" + process.env.MONGODB_ATLAS_PASSWORD;
-const mongodbName = process.env.MONGODB_NAME;
-const url = url1 + userPassword + url2 + mongodbName + url3;
-// const url = url1 + userPassword + url2 + url3;
+// const url1 = "mongodb+srv://";
+// const url2 = "@cluster0.djq235y.mongodb.net/";
+// const url3 = "?retryWrites=true&w=majority";
+// const userPassword = process.env.MONGODB_ATLAS_USER + ":" + process.env.MONGODB_ATLAS_PASSWORD;
+// const mongodbName = process.env.MONGODB_NAME;
+// const url = url1 + userPassword + url2 + mongodbName + url3;
+const url = process.env.MONGODB_URI;
 const PORT = process.env.PORT || 2000;
-mongoose.connect(url)
-    .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
-    .catch((error) => console.log(error.message));
+mongoose
+  .connect(url)
+  .then(() =>
+    app.listen(PORT, () => console.log(`Server running on port: ${PORT}`))
+  )
+  .catch((error) => console.log(error.message));
