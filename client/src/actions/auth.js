3f792b1640720c2c57af02636c6e1f38bccc0169
bxljoy <259500313@qq.com>
2024-03-27 14:58:32 +0100

    Refactor signin and signup actions to handle errors

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -1,26 +1,28 @@
 import * as api from "../api";
 import { AUTH } from "../constants/actionTypes";
 
-export const signin = (formData, navigate) => async (dispatch) => {
-    try {
-        const { data } = await api.signIn(formData);
+export const signin = (formData, navigate, setError) => async (dispatch) => {
+  try {
+    const { data } = await api.signIn(formData);
 
-        dispatch({ type: AUTH, data });
+    dispatch({ type: AUTH, data });
 
-        navigate('/');  
-    } catch (error) {
-        console.log(error);
-    }
+    navigate("/");
+  } catch (error) {
+    console.log(error);
+    setError("Invalid credentials. Please try again.");
+  }
 };
 
-export const signup = (formData, navigate) => async (dispatch) => {
-    try {
-        const { data } = await api.signUp(formData);
+export const signup = (formData, navigate, setError) => async (dispatch) => {
+  try {
+    const { data } = await api.signUp(formData);
 
-        dispatch({ type: AUTH, data });
+    dispatch({ type: AUTH, data });
 
-        navigate('/');  
-    } catch (error) {
-        console.log(error);
-    }
+    navigate("/");
+  } catch (error) {
+    console.log(error);
+    setError("Invalid credentials. Please try again.");
+  }
 };
--- client/src/components/Auth/Auth.js
+++ client/src/components/Auth/Auth.js
@@ -1,195 +1,258 @@
-import React, { useState } from 'react';
-import { Avatar, Grid, Paper, Button, Typography, Container } from '@mui/material';
-import { useGoogleLogin } from '@react-oauth/google';
-import { GoogleLogin } from '@react-oauth/google';
-import LockOutLinedIcon from '@mui/icons-material/LockOutlined';
-import Input from './Input'; 
-import MyIcon from './MyIcon';
-import Box from '@mui/material/Box';
-import { useDispatch } from 'react-redux';
-import { useNavigate } from 'react-router-dom';
-import * as api from '../../api';
-import { signup, signin } from '../../actions/auth';
+import React, { useState } from "react";
+import {
+  Avatar,
+  Grid,
+  Paper,
+  Button,
+  Typography,
+  Container,
+  Alert,
+} from "@mui/material";
+import { useGoogleLogin } from "@react-oauth/google";
+import { GoogleLogin } from "@react-oauth/google";
+import LockOutLinedIcon from "@mui/icons-material/LockOutlined";
+import Input from "./Input";
+import MyIcon from "./MyIcon";
+import Box from "@mui/material/Box";
+import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import * as api from "../../api";
+import { signup, signin } from "../../actions/auth";
 
-const initialState = { firstName: '', lastName: '', email: '', password: '', confirmPassword: '' };
+const initialState = {
+  firstName: "",
+  lastName: "",
+  email: "",
+  password: "",
+  confirmPassword: "",
+};
 
 const Auth = () => {
-    const [isSignup, setIsSignup] = useState(false);
+  const [isSignup, setIsSignup] = useState(false);
 
-    const [showPassword, setShowPassword] = useState(false);
+  const [showPassword, setShowPassword] = useState(false);
 
-    const [formData, setFormData] = useState(initialState);
+  const [error, setError] = useState("");
 
-    const handleShowPassword = () => setShowPassword((prevShowPassword) => !prevShowPassword);
+  const [formData, setFormData] = useState(initialState);
 
-    const dispatch = useDispatch();
+  const handleShowPassword = () =>
+    setShowPassword((prevShowPassword) => !prevShowPassword);
 
-    const navigate = useNavigate();
+  const dispatch = useDispatch();
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        if (isSignup) {
-            dispatch(signup(formData, navigate));
-        } else {
-            dispatch(signin(formData, navigate));
-        }
-    };
+  const navigate = useNavigate();
 
-    const handleChange = (e) => {
-        setFormData({ ...formData, [e.target.name]: e.target.value });
-    };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (isSignup) {
+      dispatch(signup(formData, navigate, setError));
+    } else {
+      dispatch(signin(formData, navigate, setError));
+    }
+  };
 
-    const switchMode = () => {
-        setIsSignup((prevMode) => !prevMode);
-        setShowPassword(false);
-    };
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
 
-    const googleSuccess = async (res) => {
-        // console.log(res);
-        try {
-            const { data: result } = await api.verifyJwtToken(res);
-            // console.log(result);
-            const token = res.credential;
-            // console.log(`token: ${token}`);
-            dispatch({type: 'AUTH', data: { result, token }});
-            navigate('/');
-        } catch (error) {
-            console.log(error);
-        }
-    };
+  const switchMode = () => {
+    setIsSignup((prevMode) => !prevMode);
+    setShowPassword(false);
+  };
 
-    const googleFailure = (error) => {
-        console.log(error); 
-        console.log('Google Sign In was unsuccessful. Try Again Later');
-    };
+  const googleSuccess = async (res) => {
+    // console.log(res);
+    try {
+      const { data: result } = await api.verifyJwtToken(res);
+      // console.log(result);
+      const token = res.credential;
+      // console.log(`token: ${token}`);
+      dispatch({ type: "AUTH", data: { result, token } });
+      navigate("/");
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
-    // calling google api from frontend, it's not safe enough.
-    // const login = useGoogleLogin({
-    //     onSuccess: async tokenResponse => {
-    //       console.log(tokenResponse);
-    //       // fetching userinfo can be done on the client or the server
-    //       const userInfo = await axios
-    //         .get('https://www.googleapis.com/oauth2/v3/userinfo', {
-    //           headers: { Authorization: `Bearer ${tokenResponse.access_token}` },
-    //         })
-    //         .then(res => res.data);
-    //       console.log(userInfo);
-    //     },
-    //     // flow: 'implicit', // implicit is the default
-    //     onError: googleFailure,
-    // });
+  const googleFailure = (error) => {
+    console.log(error);
+    console.log("Google Sign In was unsuccessful. Try Again Later");
+  };
 
-    // const loginAndGetTokens = useGoogleLogin({
-    //     onSuccess: async (tokenResponse) => {
-    //         console.log(tokenResponse);
-    //         const tokens = await api.getTokens({
-    //             code: tokenResponse.code,
-    //         });
-        
-    //         console.log(tokens);
-    //     },
-    //     flow: 'auth-code',     // 'auto-code' flow can get tokenResponse.code, the default flow 'implicit' can get tokenResponse.access_token
-    //     onError: googleFailure,
-    // });
+  // calling google api from frontend, it's not safe enough.
+  // const login = useGoogleLogin({
+  //     onSuccess: async tokenResponse => {
+  //       console.log(tokenResponse);
+  //       // fetching userinfo can be done on the client or the server
+  //       const userInfo = await axios
+  //         .get('https://www.googleapis.com/oauth2/v3/userinfo', {
+  //           headers: { Authorization: `Bearer ${tokenResponse.access_token}` },
+  //         })
+  //         .then(res => res.data);
+  //       console.log(userInfo);
+  //     },
+  //     // flow: 'implicit', // implicit is the default
+  //     onError: googleFailure,
+  // });
 
-    const loginAndGetUserInfo = useGoogleLogin({
-        flow: 'auth-code',     // 'auto-code' flow can get tokenResponse.code, the default flow 'implicit' can get tokenResponse.access_token
-        onSuccess: async (tokenResponse) => {
-            // console.log(tokenResponse);
-            try {
-                const { data: result } = await api.getUserInfo({
-                    code: tokenResponse.code,
-                });
-                // console.log(result);
-                const token = result.id_token;
-                // console.log(`token: ${token}`);
-                dispatch({type: 'AUTH', data: { result, token }});
-                navigate('/');
-            } catch (error) {
-                console.log(error);
-            }
-        },
-        onError: googleFailure,
-    });
+  // const loginAndGetTokens = useGoogleLogin({
+  //     onSuccess: async (tokenResponse) => {
+  //         console.log(tokenResponse);
+  //         const tokens = await api.getTokens({
+  //             code: tokenResponse.code,
+  //         });
 
+  //         console.log(tokens);
+  //     },
+  //     flow: 'auth-code',     // 'auto-code' flow can get tokenResponse.code, the default flow 'implicit' can get tokenResponse.access_token
+  //     onError: googleFailure,
+  // });
 
-    return (
-        <Container component="main" maxWidth="xs">
-            <Paper 
-                sx={{
-                    marginTop: (theme) => theme.spacing(8),
-                    display: 'flex',
-                    flexDirection: 'column',
-                    alignItems: 'center',
-                    padding: (theme) => theme.spacing(2),
-                }}
-                elevation={3}
-            >
-                <Avatar 
-                    sx={{
-                        margin: (theme) => theme.spacing(1),
-                        backgroundColor: (theme) => theme.palette.secondary.main,
-                    }}
-                >
-                    <LockOutLinedIcon />
-                </Avatar>
-                <Typography variant='h5'>{isSignup ? 'Sign Up' : 'Sign In'}</Typography>
-                <Box 
-                    component="form" 
-                    sx={{
-                        width: '100%', // Fix IE 11 issue.
-                        marginTop: (theme) => theme.spacing(3),
-                    }}
-                    onSubmit={handleSubmit}>
-                    <Grid container spacing={2}
-                >
-                        {
-                            isSignup && (
-                                <>
-                                    <Input name='firstName' label='First Name' handleChange={handleChange} autoFocus half />
-                                    <Input name='lastName' label='Last Name' handleChange={handleChange} half />
-                                </>
-                            )
-                        }
-                        <Input name='email' label='Email Address' handleChange={handleChange} type='email'  />
-                        <Input name='password' label='Password' handleChange={handleChange} type={showPassword ? 'text' : 'password'} handleShowPassword={handleShowPassword} />
-                        { isSignup && <Input name='confirmPassword' label='Repeat Password' handleChange={handleChange} type='password' /> }
-                    </Grid>
-                    <Button 
-                        type='submit' 
-                        fullWidth 
-                        variant='contained' 
-                        color='primary' 
-                        sx={{
-                            margin: (theme) => theme.spacing(3, 0, 2),
-                        }}
-                    >
-                        {isSignup ? 'Sign Up' : 'Sign In'}
-                    </Button>
-                    <Button 
-                        sx={{
-                            marginBottom: (theme) => theme.spacing(2),
-                        }}
-                        color='primary' 
-                        fullWidth 
-                        onClick={loginAndGetUserInfo} 
-                        startIcon={<MyIcon />} 
-                        variant='contained'
-                    >
-                        Sign in with Google 🚀{' '}
-                    </Button>
-                    <GoogleLogin onSuccess={googleSuccess} onError={googleFailure} size='large' auto_select useOneTap />
-                    <Grid container justifyContent='flex-end'>
-                        <Grid item>
-                            <Button onClick={switchMode}>
-                                { isSignup ? 'Already have an account? Sign In' : "Don't have an account? Sign Up" }
-                            </Button>
-                        </Grid>
-                    </Grid>
-                </Box>
-            </Paper>
-        </Container>
-    )
-}
+  const loginAndGetUserInfo = useGoogleLogin({
+    flow: "auth-code", // 'auto-code' flow can get tokenResponse.code, the default flow 'implicit' can get tokenResponse.access_token
+    onSuccess: async (tokenResponse) => {
+      // console.log(tokenResponse);
+      try {
+        const { data: result } = await api.getUserInfo({
+          code: tokenResponse.code,
+        });
+        // console.log(result);
+        const token = result.id_token;
+        // console.log(`token: ${token}`);
+        dispatch({ type: "AUTH", data: { result, token } });
+        navigate("/");
+      } catch (error) {
+        console.log(error);
+      }
+    },
+    onError: googleFailure,
+  });
+
+  return (
+    <Container component="main" maxWidth="xs">
+      <Paper
+        sx={{
+          marginTop: (theme) => theme.spacing(8),
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          padding: (theme) => theme.spacing(2),
+        }}
+        elevation={3}
+      >
+        <Avatar
+          sx={{
+            margin: (theme) => theme.spacing(1),
+            backgroundColor: (theme) => theme.palette.secondary.main,
+          }}
+        >
+          <LockOutLinedIcon />
+        </Avatar>
+        <Typography variant="h5">{isSignup ? "Sign Up" : "Sign In"}</Typography>
+        {error && (
+          <Alert
+            variant="filled"
+            severity="error"
+            onClose={() => {
+              setError("");
+            }}
+            sx={{ marginTop: "10px" }}
+          >
+            {error}
+          </Alert>
+        )}
+        <Box
+          component="form"
+          sx={{
+            width: "100%", // Fix IE 11 issue.
+            marginTop: (theme) => theme.spacing(3),
+          }}
+          onSubmit={handleSubmit}
+        >
+          <Grid container spacing={2}>
+            {isSignup && (
+              <>
+                <Input
+                  name="firstName"
+                  label="First Name"
+                  handleChange={handleChange}
+                  autoFocus
+                  half
+                />
+                <Input
+                  name="lastName"
+                  label="Last Name"
+                  handleChange={handleChange}
+                  half
+                />
+              </>
+            )}
+            <Input
+              name="email"
+              label="Email Address"
+              handleChange={handleChange}
+              type="email"
+            />
+            <Input
+              name="password"
+              label="Password"
+              handleChange={handleChange}
+              type={showPassword ? "text" : "password"}
+              handleShowPassword={handleShowPassword}
+            />
+            {isSignup && (
+              <Input
+                name="confirmPassword"
+                label="Repeat Password"
+                handleChange={handleChange}
+                type="password"
+              />
+            )}
+          </Grid>
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+            sx={{
+              margin: (theme) => theme.spacing(3, 0, 2),
+            }}
+          >
+            {isSignup ? "Sign Up" : "Sign In"}
+          </Button>
+          <Button
+            sx={{
+              marginBottom: (theme) => theme.spacing(2),
+            }}
+            color="primary"
+            fullWidth
+            onClick={loginAndGetUserInfo}
+            startIcon={<MyIcon />}
+            variant="contained"
+          >
+            Sign in with Google 🚀{" "}
+          </Button>
+          <GoogleLogin
+            onSuccess={googleSuccess}
+            onError={googleFailure}
+            size="large"
+            auto_select
+            useOneTap
+          />
+          <Grid container justifyContent="flex-end">
+            <Grid item>
+              <Button onClick={switchMode}>
+                {isSignup
+                  ? "Already have an account? Sign In"
+                  : "Don't have an account? Sign Up"}
+              </Button>
+            </Grid>
+          </Grid>
+        </Box>
+      </Paper>
+    </Container>
+  );
+};
 
 export default Auth;
