b0bf488f95d1dea6a7fe587b00bb43d4291ef5da
bxljoy <259500313@qq.com>
2024-03-30 21:58:22 +0100

    Add cookieSecure middleware and update routes

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,16 +1,26 @@
-import express from 'express';
-import { getPosts, getPost, getPostsBySearch, createPost, updatePost, deletePost, likePost, commentPost } from '../controllers/posts.js';
-import auth from '../middleware/auth.js';
+import express from "express";
+import {
+  getPosts,
+  getPost,
+  getPostsBySearch,
+  createPost,
+  updatePost,
+  deletePost,
+  likePost,
+  commentPost,
+} from "../controllers/posts.js";
+import auth from "../middleware/auth.js";
+import cookieSecure from "../middleware/cookieSecure.js";
 
 const router = express.Router();
 
-router.get('/search', getPostsBySearch);
-router.get('/', getPosts);
-router.get('/:id', getPost);
-router.post('/', auth, createPost);
-router.patch('/:id', auth, updatePost);
-router.delete('/:id', auth, deletePost);
-router.patch('/:id/likePost', auth, likePost);
-router.post('/:id/commentPost', auth, commentPost);
+router.get("/search", getPostsBySearch);
+router.get("/", cookieSecure, getPosts);
+router.get("/:id", getPost);
+router.post("/", auth, createPost);
+router.patch("/:id", auth, updatePost);
+router.delete("/:id", auth, deletePost);
+router.patch("/:id/likePost", auth, likePost);
+router.post("/:id/commentPost", auth, commentPost);
 
 export default router;
